e41a62db2abf698ef4f54da15ef2e7b61eea3943
Eric Boivin <ericboivin@gmail.com>
2018-02-06 01:17:08 -0500

    Renamed case-sensitive files

--- api/app.js
+++ api/app.js
@@ -3,13 +3,13 @@ var app = express();
 
 var db = require('./db');
 
-var StickerController = require('./sticker/stickerController');
+var StickerController = require('./sticker/StickerController');
 app.use('/sticker', StickerController);
 
-var ReservationController = require('./reservation/reservationController');
+var ReservationController = require('./reservation/ReservationController');
 app.use('/reservation', ReservationController);
 
-var StudentController = require('./student/studentController');
+var StudentController = require('./student/StudentController');
 app.use('/student', StudentController);
 
 module.exports = app;
